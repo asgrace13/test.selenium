7a88f4b753890c4d4e3ca59b8eea2a112ac73836
asgrace13 <asgrace13@gmail.com>
2017-10-09 00:15:49 +0300

    Отредактированы Договры поставки с учетом новых функций ядра

--- fixture/agreement.py
+++ fixture/agreement.py
@@ -9,59 +9,56 @@ class AgreementHelper:
 
     def open_supply_agreement_page(self):
         wd = self.app.wd
-        time.sleep(5)
         if not wd.current_url.endswith("lk/#/contracts/supplyAgreement"):
-            wd.get("http://192.168.242.167:8080/lk/#/contracts/supplyAgreement")
-            time.sleep(5)
+            wd.get(self.app.base_url + "/lk/#/contracts/supplyAgreement")
+            self.app.wait_for_page_load()
 
     def create(self, Agreement):
         wd = self.app.wd
         self.open_supply_agreement_page()
         self.add_new_supply_agreement()
+        self.fill_agreement_form(Agreement)
+        # проверка создания
+        self.app.element_is_hidden((By.NAME, "LegalPerson"), timeout=30)
+        
+
+    def fill_agreement_form(self, Agreement):
         # wd.find_element(By.CLASS_NAME, "dk-lpform__legal-form-select").click()
         # select Agreement.type
-        wd.find_element(By.NAME, "LegalPerson").click()
-        wd.find_element(By.NAME, "LegalPerson").clear()
-        wd.find_element(By.NAME, "LegalPerson").send_keys(Agreement.organization)
-        wd.find_element(By.NAME, "INN").click()
-        wd.find_element(By.NAME, "INN").clear()
-        wd.find_element(By.NAME, "INN").send_keys(Agreement.inn)
-        wd.find_element(By.NAME, "KPP").click()
-        wd.find_element(By.NAME, "KPP").clear()
-        wd.find_element(By.NAME, "KPP").send_keys(Agreement.kpp)
-        wd.find_element(By.NAME, "BIK").click()
-        wd.find_element(By.NAME, "BIK").clear()
-        wd.find_element(By.NAME, "BIK").send_keys(Agreement.bik)
-        wd.find_element(By.NAME, "CorrespondentAccount").click()
-        wd.find_element(By.NAME, "CorrespondentAccount").clear()
-        wd.find_element(By.NAME, "CorrespondentAccount").send_keys(Agreement.correspondent_account)
-        wd.find_element(By.NAME, "SettlementAccount").click()
-        wd.find_element(By.NAME, "SettlementAccount").clear()
-        wd.find_element(By.NAME, "SettlementAccount").send_keys(Agreement.checking_account)
-        wd.find_element(By.NAME, "HeadPosition").click()
-        wd.find_element(By.NAME, "HeadPosition").clear()
-        wd.find_element(By.NAME, "HeadPosition").send_keys(Agreement.position)
-        wd.find_element(By.NAME, "HeadSecondName").click()
-        wd.find_element(By.NAME, "HeadSecondName").clear()
-        wd.find_element(By.NAME, "HeadSecondName").send_keys(Agreement.surname)
-        wd.find_element(By.NAME, "HeadFirstName").click()
-        wd.find_element(By.NAME, "HeadFirstName").clear()
-        wd.find_element(By.NAME, "HeadFirstName").send_keys(Agreement.name)
-        wd.find_element(By.NAME, "HeadMiddleName").click()
-        wd.find_element(By.NAME, "HeadMiddleName").clear()
-        wd.find_element(By.NAME, "HeadMiddleName").send_keys(Agreement.patronymic)
-        wd.find_element(By.NAME, "LegalAddress").click()
-        wd.find_element(By.NAME, "LegalAddress").clear()
-        wd.find_element(By.NAME, "LegalAddress").send_keys(Agreement.address)
-        wd.find_element(By.NAME, "Phone").click()
-        wd.find_element(By.NAME, "Phone").clear()
-        wd.find_element(By.NAME, "Phone").send_keys(Agreement.phone)
-        wd.find_element(By.NAME, "Email").click()
-        wd.find_element(By.NAME, "Email").clear()
-        wd.find_element(By.NAME, "Email").send_keys(Agreement.email)
-        wd.find_element(By.ID, "submit").click()
-        time.sleep(10)
-        # проверка создания
+        organization_locator = (By.NAME, "LegalPerson")
+        self.app.change_field_value(locator=organization_locator, text=Agreement.organization)
+        inn_locator = (By.NAME, "INN")
+        self.app.change_field_value(locator=inn_locator, text=Agreement.inn)
+        kpp_locator = (By.NAME, "KPP")
+        self.app.change_field_value(locator=kpp_locator, text=Agreement.kpp)
+        bik_locator = (By.NAME, "BIK")
+        self.app.change_field_value(locator=bik_locator, text=Agreement.bik)
+        correspondent_account_locator = (By.NAME, "CorrespondentAccount")
+        self.app.change_field_value(locator=correspondent_account_locator, text=Agreement.correspondent_account)
+        checking_account_locator = (By.NAME, "SettlementAccount")
+        self.app.change_field_value(locator=checking_account_locator, text=Agreement.checking_account)
+        position_locator = (By.NAME, "HeadPosition")
+        self.app.change_field_value(locator=position_locator, text=Agreement.position)
+        surname_locator = (By.NAME, "HeadSecondName")
+        self.app.change_field_value(locator=surname_locator, text=Agreement.surname)
+        name_locator = (By.NAME, "HeadFirstName")
+        self.app.change_field_value(locator=name_locator, text=Agreement.name)
+        patronymic_locator = (By.NAME, "HeadMiddleName")
+        self.app.change_field_value(locator=patronymic_locator, text=Agreement.patronymic)
+        address_locator = (By.NAME, "LegalAddress")
+        self.app.change_field_value(locator=address_locator, text=Agreement.address)
+        phone_locator = (By.NAME, "Phone")
+        self.app.change_field_value(locator=phone_locator, text=Agreement.phone)
+        email_locator = (By.NAME, "Email")
+        self.app.change_field_value(locator=email_locator, text=Agreement.email)
+        self.app.element_is_clickable(By.ID, "submit").click()
+
+
+    def change_field_value(self, locator, text):
+        wd = self.app.wd
+        wd.find_element(*locator).click()
+        wd.find_element(*locator).send_keys(text)
+
 
     def add_new_supply_agreement(self):
         wd = self.app.wd
--- test/test_create_supply_agreement.py
+++ test/test_create_supply_agreement.py
@@ -3,4 +3,5 @@
 
 def test_create_supply_agreement(app, json_agreements):
     agreement = json_agreements
+    app.session.ensure_login(email=app.target["userEmail"], password=app.target["userPassword"])
     app.agreement.create(agreement)
